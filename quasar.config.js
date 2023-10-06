2616519d4a563a3b4b1f22f9da4a266ee956adfc
jumyeong33 <jm01028250379@gmail.com>
2023-10-06 14:28:35 +0900

    [Modify] Using Session storage

--- quasar.config.js
+++ quasar.config.js
@@ -111,7 +111,7 @@ module.exports = configure(function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: ['Notify']
+      plugins: ['Notify', 'SessionStorage']
     },
 
     animations: 'all', // --- includes all animations
