5245495d181cb4c45caf40e4fb10a5f3c58c36a2
jumyeong33 <jm01028250379@gmail.com>
2023-11-06 19:37:31 +0900

    [Add & Modify] genertator, constant mapping
    - mapping for making attribute in metadata
    - generator for uniquekey and metadata

---
+++ constant.js
@@ -0,0 +1,43 @@
+export const EmojiMapping = {
+    101 : "Lucky Clover",
+    102 : "Red Apple",
+    103 : "Blue Heart",
+    104 : "Cloud",
+    105 : "Triple Clover",
+    106 : "Overlapped Heart",
+    107 : "Daisy",
+    108 : "Fried Egg",
+    109 : "Exclamation Heart",
+    110 : "Glittering",
+    111 : "Green Apple",
+    112 : "Green Heart",
+    113 : "Lightning",
+    114 : "Quince",
+    115 : "Crescent",
+    116 : "Orange Heart",
+    117 : "Cactus",
+    118 : "Rain Cloud",
+    119 : "Rainbow",
+    120 : "Red Heart",
+    121 : "Red Tulip",
+    122 : "Pink Tulip",
+    123 : "Sad Face",
+    124 : "Smile Face",
+    125 : "Sun & Cloud",
+    126 : "Sun",
+    127 : "Yellow Heart",
+}
+
+export const BackgroundMapping = {
+    0 : "Neon Green",
+    1 : "Neon Pink",
+    2 : "Neon Blue",
+    3 : "Indigo",
+    4 : "Black",
+}
+
+export const PatternMapping = {
+    1 : "Pattern 1",
+    2 : "Pattern 2",
+    3 : "Pattern 3",
+}
