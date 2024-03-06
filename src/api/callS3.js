ebeb4920fe938016e7ca7bdcaf1b0989bc64ecc6
jumyeong33 <jm01028250379@gmail.com>
2024-03-06 20:21:59 +0900

    [Temp] testing on prod

--- src/api/callS3.js
+++ src/api/callS3.js
@@ -1,23 +1,28 @@
-import { S3Client, ListObjectsCommand } from '@aws-sdk/client-s3';
+import { S3Client, ListObjectsCommand } from "@aws-sdk/client-s3";
 
 class ClientS3 {
-    #client
-    
-    constructor() {
-        this.#client = new S3Client({
-            region:'ap-northeast-2',
-            credentials: {
-                accessKeyId: process.env.S3_ACCESS_KEY,
-                secretAccessKey: process.env.S3_SECRET_KEY,
-            }
-        });
-    }
+  #client;
 
-    async getListOfBucket(prefix) {
-        const command = new ListObjectsCommand({ Bucket: 'dduckddack-test', Prefix : prefix});
-        const response = await this.#client.send(command);
-        return response.Contents;
-    }
+  constructor() {
+    this.#client = new S3Client({
+      region: "ap-northeast-2",
+      credentials: {
+        accessKeyId: process.env.S3_ACCESS_KEY,
+        secretAccessKey: process.env.S3_SECRET_KEY,
+      },
+    });
+  }
+
+  async getListOfBucket(prefix) {
+    console.log("process.env.S3_ACCESS_KEY", process.env.S3_ACCESS_KEY);
+    console.log("process.env.S3_SECRET_KEY", process.env.S3_SECRET_KEY);
+    const command = new ListObjectsCommand({
+      Bucket: "dduckddack-test",
+      Prefix: prefix,
+    });
+    const response = await this.#client.send(command);
+    return response.Contents;
+  }
 }
 
 export default new ClientS3();
